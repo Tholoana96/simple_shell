4fa6860b4970082d954f1d400f42cd7ef6ab857c
Nomcebo1992 <snembambo@gmail.com>
2023-12-13 07:59:15 -0800

    path.c

---
+++ path.c
@@ -0,0 +1,24 @@
+#include "shell.h"
+
+/**
+ * parse_path - Parse the PATH variable and return an array of directories.
+ *
+ * Return: An array of directories.
+ */
+return (strsep(&path, ":"));
+{
+	char **dirs = NULL;
+	char *tokens[PATH_MAX];
+	size_t i = 0;
+
+		if (path == NULL || strlen(path) == 0)
+		return (NULL);
+else
+			dirs = malloc(sizeof(char *) * n);
+			if (dirs == NULL)
+			return (NULL);
+return (NULL);
+			}
+		}
+return (dirs);
+}
